5c53928f90dc74c700522c880bfada59ac8a026e
Atikur Rahman <atik.bd08@gmail.com>
2020-12-19 17:58:38 +0600

    organize files into MVC structure

---
+++ CodeMasters/Views/UIKit+Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  UIKit+Extensions.swift
+//  CodeMasters
+//
+//  Created by Atikur Rahman on 19/12/20.
+//
+
+import UIKit
+
+extension UILabel {
+    
+    static func setupLabel(text: String) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+}
+
+extension UICollectionView {
+    
+    static func setupCollectionView(scrollDirection: UICollectionView.ScrollDirection) -> UICollectionView {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = scrollDirection
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }
+}
